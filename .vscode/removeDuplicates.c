a982d3db99e4833ea5f8b9d0280962a4312e16d0
oliver <2822738182@qq.com>
2024-07-25 10:20:30 +0800

    leetcode

---
+++ .vscode/removeDuplicates.c
@@ -0,0 +1,15 @@
+int removeDuplicates(int* nums, int numsSize)
+{
+    int slow=1,fast=1;
+    for(;fast<numsSize;fast++)
+    {
+        //这里出现的问题就是如果fast是最后一个元素，数组再加一就没有对应的元素了
+        if(nums[fast]!=nums[fast-1])
+        {
+            //如果不修改数组中的元素，就默认保留之前存在的，不要对其重新赋值。
+            nums[slow]=nums[fast];
+            slow++;
+        }
+    }
+    return slow;
+}
