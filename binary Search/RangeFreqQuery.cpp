faee77ec8185ec71ab6c0547c0b56bfa07b5f459
oliver <2822738182@qq.com>
2025-12-20 12:04:45 +0800

    leetcode

---
+++ binary Search/RangeFreqQuery.cpp
@@ -0,0 +1,29 @@
+class RangeFreqQuery {
+
+private:
+    unordered_map<int,vector<int>> cnt;
+public:
+    RangeFreqQuery(vector<int>& arr)
+    {
+        int n=arr.size();
+        for(int i=0;i<n;++i)
+        {
+            cnt[arr[i]].push_back(i);
+        }
+    }
+    
+    int query(int left, int right, int value)
+    {
+        const vector<int>&pos=cnt[value];
+
+        auto l=lower_bound(pos.begin(),pos.end(),left);
+        auto r=upper_bound(pos.begin(),pos.end(),right);
+        return r-l;
+    }
+};
+
+/**
+ * Your RangeFreqQuery object will be instantiated and called as such:
+ * RangeFreqQuery* obj = new RangeFreqQuery(arr);
+ * int param_1 = obj->query(left,right,value);
+ */
